aae8e34cb1af084c308c8e28e6fb8b21150592bf
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-02-10 17:04:56 +0000

    data

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import sqlite3
+
 conn  = sqlite3.connect("database.db")
 c = conn.cursor()
-c.execute("""
-CREATE TABLE IF NOT EXISTS informations
-(
-    nom text,
-    prenom text,
-    email text,
-    mdp text,
-    confirm mdp text,  
-)
-""")
+c.execute("""CREATE TABLE IF NOT EXISTS informations(   
+        nom text,
+        prenom text,
+        email text, 
+        mdp text, 
+        confirm text 
+    )""")
 
-d = {"nom":"Kouaho", "prenom":"Jaheim", "email":"jaheimkouaho@gmail.com", "mdp":"Jaheim2002", "confirm mdp":"Jaheim2002"}
-c.execute("INSERT INTO informations VALUES(:nom, :prenom, :email, :mdp , :confirm mdp", d)
+d = {"nom": "Kouaho" , "prenom" : "Jaheim" , "email":"jaheimkouaho@gmail.com", "mdp":"hj", "confirm":"hj"}
+c.execute("INSERT INTO informations VALUES(:nom, :prenom, :email, :mdp, :confirm)", d)
+
+c.execute("SELECT * FROM informations ")
+donnes = c.fetchall()
+print(donnes)
+
 
 conn.commit()
 conn.close()
